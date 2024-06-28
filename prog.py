57df3d0907d6bc65af60df5d5c3cc41e2a8c2aee
yabje <75222926+yabje@users.noreply.github.com>
2024-06-28 22:54:27 +0200

    ask for bsr

--- prog.py
+++ prog.py
@@ -1,4 +1,10 @@
 import time
 
-print("Hello world")
+# Ask the user to input a map code
+bsr = input("Please enter the map key: ")
+
+# Print the map code to confirm it has been stored correctly
+print(f"The entered map key is: {bsr}")
+
+# Wait 10 seconds before closing
 time.sleep(10)
