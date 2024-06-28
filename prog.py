0066ec19af91e66872247ec87d77dc2d3822df33
yabje <75222926+yabje@users.noreply.github.com>
2024-06-28 23:18:11 +0200

    added start values

--- prog.py
+++ prog.py
@@ -1,10 +1,43 @@
 import time
+import requests
+from tkinter import *
 
-# Ask the user to input a map code
-bsr = input("Please enter the map key: ")
+def submit():
 
-# Print the map code to confirm it has been stored correctly
-print(f"The entered map key is: {bsr}")
+    global bsr
+    bsr = str(mapKey_entry.get())
 
-# Wait 10 seconds before closing
-time.sleep(10)
+    # Print the map code to confirm it has been stored correctly
+    print(f"The entered map key is: {bsr}")
+
+
+    # Api call to beatsaver
+    r = requests.get(f"https://api.beatsaver.com/maps/id/{bsr}")
+
+    if r.status_code == 200:
+        data = r.json()
+        mapName = data['name']
+    else:
+        print(f"Failed to get map info for {bsr}. Status code: {r.status_code}")
+
+    # Print the map name
+    print(mapName)
+
+    # Wait 10 seconds before closing
+    time.sleep(10)
+
+
+root = Tk()
+root.title("SSRM Automator")
+root.geometry('600x400')
+
+mapKey_label = Label(root, text="Map key:")
+mapKey_label.pack()
+mapKey_entry = Entry(root)
+mapKey_entry.pack()
+
+submit_button = Button(root, text= "Submit", command=submit)
+submit_button.pack()
+
+# Print GUI
+root.mainloop()
