74f66598e3fd96d6800e3d16d2ac2a604e552d8b
yabje <75222926+yabje@users.noreply.github.com>
2024-06-29 14:35:42 +0200

    QOL fixes, including copy button and you can use enter to submit map key now :D

--- prog.py
+++ prog.py
@@ -59,8 +59,10 @@ def on_closing():
 # Function to adjust the position of the copy buttons
 def adjust_copy_button_positions(event):
     x_pos = root.winfo_width() - 40  # 10 pixels from the right edge, considering the button width
-    copy_title_button.place(x=x_pos, y=110)  # Adjust y position as necessary
-    copy_description_button.place(x=x_pos, y=240)  # Adjust y position as necessary
+    title_button_y_pos = title_text.winfo_y() - 30  # 30 pixels above the title text box
+    description_button_y_pos = description_text.winfo_y() - 30  # 30 pixels above the description text box
+    copy_title_button.place(x=x_pos, y=title_button_y_pos)
+    copy_description_button.place(x=x_pos, y=description_button_y_pos)
 
 # Create the main window
 root = Tk()
@@ -74,24 +76,32 @@ mapKey_label.pack(pady=10)
 mapKey_entry = ttk.Entry(root)
 mapKey_entry.pack(pady=10)
 
+# Bind the Enter key press events to submit the map key
+mapKey_entry.bind('<Return>', lambda event: submit())
+mapKey_entry.bind('<KP_Enter>', lambda event: submit())
+
 # Create and pack the submit button
 submit_button = ttk.Button(root, text="Submit", command=submit)
 submit_button.pack(pady=10)
 
 # Create and pack the text widget to display the title
-title_text = Text(root, height=2, wrap=WORD)
+title_text = Text(root, height=1, wrap=WORD)
 title_text.pack(pady=10, fill=BOTH, expand=True)
 title_text.config(state=DISABLED)  # Make text box read-only
 
 # Load the copy image
 copy_image_path = "Images/copy-icon.png"
 copy_image = Image.open(copy_image_path)
-copy_image = copy_image.resize((18, 18), Image.Resampling.LANCZOS)  # Resize if necessary
+copy_image = copy_image.resize((18, 18), Image.LANCZOS)  # Resizer
 copy_image = ImageTk.PhotoImage(copy_image)
 
 # Create and place the copy image button for title
 copy_title_button = Label(root, image=copy_image, cursor="hand2")
-copy_title_button.place(x=560, y=110)  # Initial position; will be adjusted
+copy_title_button.place(x=560, y=80)  # Initial position; will be adjusted
+
+# Create a spacer frame
+spacer = Frame(root, height=40)  # Spacer with height 40 pixels
+spacer.pack(fill=X)
 
 # Create and pack the text widget to display the description
 description_text = Text(root, height=5, wrap=WORD)
@@ -100,7 +110,7 @@ description_text.config(state=DISABLED)  # Make text box read-only
 
 # Create and place the copy image button for description
 copy_description_button = Label(root, image=copy_image, cursor="hand2")
-copy_description_button.place(x=560, y=240)  # Initial position; will be adjusted
+copy_description_button.place(x=560, y=210)  # Initial position; will be adjusted
 
 # Bind the window resize event to adjust the position of the copy buttons
 root.bind('<Configure>', adjust_copy_button_positions)
